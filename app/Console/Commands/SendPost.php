40503abc937611b482e7974dcca1c59ef20aa6a0
Marylu Ramirez Nieto <merryrn18@gmail.com>
2019-12-08 14:22:34 -0600

    comments
    comments

--- app/Console/Commands/SendPost.php
+++ app/Console/Commands/SendPost.php
@@ -6,6 +6,10 @@ use Illuminate\Console\Command;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
+/* About questions 4 and 5, I decided to use the library Guzzle because with it I can be sure the request will send it and also the library has the support to handle many requests and the security to take the correct responses from the page.
+I added a try-catch method to be sure the page will not crash if some of the answers is not correct.
+About question 5, I think the best way to garantee an answer or guarantee the system is sending a request is using the libraries and tools that we can use with laravel because you can have a strong system, more efficient and also the code is cleaner and faster to write. */
+
 class SendPost extends Command
 {
     /**
